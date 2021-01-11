a6a93ca0852c75fbf7de7de1ff53fb6250e6160b
Amy <amytang77@gmail.com>
2021-01-11 14:50:47 -0500

    setting up gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,43 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Strata by HTML5 UP',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js Starter based on Strata by HTML5 UP',
+    title: 'Amy Tang Codes',
+    author: 'Amy Tang',
+    description: 'Another software developer portfolio site!',
+    header: {
+      description: 'Another software developer portfolio site!',
+    },
+    updateDescription:
+      "Since I've only been programming for about half a year and I like to move quickly, I need to review the basics. Oftentimes, I find myself not remembering how to do certain things like setting up a GraphQL server or setting some Typescript types. Also, I would like to revisit algorithms and data structures as I haven't mastered them 100%. Here are the basics that I'm going to review in 2021:",
+    //     Setting up a Node.js Typescript backend
+    // Algorithms and data structures
+    // Big O Notation
+    // Server-side rendering
+    // Typescript
+    // Authentication with JWTs"
+    footer: [
+      {
+        href: 'https://www.linkedin.com/in/amytangcodes/',
+        label: 'LinkedIn',
+      },
+      {
+        href: 'https://github.com/amytangcodes',
+        label: 'Github',
+      },
+      {
+        href: 'mailto:hello@amytangcodes.com',
+        label: 'Email',
+      },
+    ],
+    menu: [
+      {
+        label: 'About',
+        path: '/',
+      },
+      {
+        label: 'Resume',
+        path: '/resume/',
+      },
+    ],
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Helmet from 'react-helmet'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
-import Cover from '../components/Cover'
+import Header from '../components/Header'
 
-const HomeIndex = () => {
-  return (
-    <Layout>
-      <Cover />
-    </Layout>
-  )
+class HomeIndex extends Component {
+  render() {
+    const { title, description } = this.props.data.site.siteMetadata
+    const { children } = this.props
+
+    console.log(children)
+    return (
+      <Layout>
+        <div>
+          <Helmet>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+          </Helmet>
+          <Header {...this.props} />
+          {/* <div id="main">{children}</div> */}
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
+
+export const query = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+        author
+        description
+        footer {
+          href
+          label
+        }
+        menu {
+          label
+          path
+        }
+      }
+    }
+  }
+`
