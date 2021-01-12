e17f9b06453fdf271a46a845a5329c40570fc366
Amy <amytang77@gmail.com>
2021-01-12 15:17:35 -0500

    cover update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,20 +8,17 @@ const HomeIndex = () => {
         <p>
           I'm a Software Developer with a mix of experience working on web
           applications. I'm currently interested in roles with a focus on
-          working with current Javascript frameworks as I do think my strengths
-          are in UI development, but I also want to continue my experiences in
-          backend development.
+          full-stack web development.
         </p>
-        <p>
-          <a href="mailto:hello@amytangcodes.com">Connect with me</a> for
-          collaboration and networking.
-        </p>
+        <p>I have experiences in the following:</p>
         <ul>
           <li>HTML5, CSS3</li>
-          <li>React</li>
-          <li>Ruby on Rails </li>
-          <li>Phoenix/ Elixir</li>
-          <li>Relational databases (PostgreSQL, MSSQL, etc.)</li>
+          <li>React, NodeJS, Gatsby</li>
+          <li>Ruby on Rails, Phoenix/ Elixir</li>
+          <li>
+            Relational databases (PostgreSQL, MySQL), NoSQL databases (MongoDB)
+          </li>
+          <li></li>
           <li>Postman</li>
           <li>Github, Terminal</li>
         </ul>
