f028dfa7406bae06e3667fd0da1f88d1863579e6
Amy <amytang77@gmail.com>
2021-01-12 16:14:11 -0500

    update cover for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,6 @@ module.exports = {
           name: 'About',
           path: '/',
         },
-        {
-          name: 'Resume',
-          path: '/resume/',
-        },
       ],
       footerLinks: [
         {
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const HomeIndex = () => {
           applications. I'm currently interested in roles with a focus on
           full-stack web development.
         </p>
-        <p>I have experiences in the following:</p>
+        <p>I've dabbled in the following:</p>
         <ul>
           <li>HTML5, CSS3</li>
           <li>React, NodeJS, Gatsby</li>
@@ -21,6 +21,21 @@ const HomeIndex = () => {
           <li>Postman</li>
           <li>Github, Terminal</li>
         </ul>
+        <p>
+          2020 has been quite the year. After being part of a major layoff, I
+          kept busy for most of 2020 renovating my house. 2021 plan is to
+          continue learning more about Software Development fundamentals by...
+        </p>
+        <p>
+          Sticking with 1-2 algorithm problems weekly at{' '}
+          <a href="https://codesignal.com/">Codesignal</a>.
+        </p>
+        <p>
+          Finish Wes Bos'{' '}
+          <a href="https://advancedreact.com/">Advanced React</a> course.
+        </p>
+        <p>Deploy a timetracker project using Devise with Rails and React.</p>
+        <p>Add projects to this website!</p>
       </section>
     </Layout>
   )
